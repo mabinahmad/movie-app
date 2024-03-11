768868c9368713e0fc606d22616070ed493a86f7
Muhammad <80458975+mabinahmad@users.noreply.github.com>
2024-03-11 22:51:44 +0300

    Update jwt.js

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 //======================================
 const generateAccessToken = (userId) => {
   return jwt.sign({ _id: userId }, process.env.ACCESS_TOKEN_SECRET, {
-    // expiresIn: "10s",
+    // expiresIn: "1d",
   });
 };
 
