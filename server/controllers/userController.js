c92a5bd5b9c70004b09bae5a43c156bea28fabec
Muhammad <80458975+mabinahmad@users.noreply.github.com>
2024-03-14 19:04:39 +0300

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,7 +10,7 @@ const {
 const {
   generatePasswordHash,
   comparePasswordHash,
-  verifyRefreshToken,
+
 } = require("../utils/bcrypt");
 
 //==================================================================
